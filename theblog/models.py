e93375c3ec79193cdd005713d7721c1856eced3a
Subhra Sankha Sardar <subhrass2017@email.iimcal.ac.in>
2021-09-01 15:52:20 +0530

    Updated changes

--- theblog/models.py
+++ theblog/models.py
@@ -10,7 +10,8 @@ class Post(models.Model):
 	title = models.CharField(max_length=255)
 	title_tag = models.CharField(max_length=255)
 	author = models.ForeignKey(User,on_delete=models.CASCADE)
-	body = RichTextField(blank=True, null=True)
+	#body = RichTextField(blank=True, null=True)
+	body = models.TextField()
 	post_date = models.DateField(auto_now_add=True)
 	likes = models.ManyToManyField(User,related_name='blog_posts')
 
